4368b6e5c2e13079eab8e1238fc7a0eacf059fc9
leerobertdyer <leerobertdyer@gmail.com>
2024-04-04 15:35:24 -0400

    feat: photo to realistic 3dmodel

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,9 +1,9 @@
 import { OrbitControls } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 import { degToRad } from "three/src/math/MathUtils";
-import "./Experience.css"
+import "./Experience.css";
 import ElephantModel from "./ElephantModel";
-import { XRButton } from "@react-three/xr";
+import { Controllers, XR, XRButton, startSession } from "@react-three/xr";
 import { useState } from "react";
 
 export default function Experience() {
@@ -11,30 +11,41 @@ export default function Experience() {
 
   return (
     <div className="mainExperienceDiv">
-      <XRButton 
-      id="enterVR"
-      mode="immersive-vr"
-      sessionInit={{ optionalFeatures: ['local-floor', 'bounded-floor', 'hand-tracking'] }}
-      onsessionstart={() => setIsImmersed(true)}
-      onsessionend={() => setIsImmersed(false)}
-      >{isImmersed ? 'Exit VR' : 'Enter VR'}</XRButton>
-    <Canvas className="canvas">
-      <OrbitControls
-        enablePan={false}
-        // set the min and max polar angle (vertical height) for the camera
-        minPolarAngle={degToRad(0)}
-        maxPolarAngle={degToRad(90)}
-        // set the min and max zoom distances for the camera
-        minDistance={1}
-        maxDistance={3}
+      <XRButton
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full absolute top-0 right-0 m-4 z-10"
+        id="enterVR"
+        mode="immersive-vr"
+        sessionInit={{
+          optionalFeatures: ["local-floor", "bounded-floor", "hand-tracking"],
+        }}
+        onClick={() => startSession('immersive-vr')}
+      >
+        {isImmersed ? "Exit VR" : "Enter VR"}
+      </XRButton>
+      <Canvas className="canvas">
+        <OrbitControls
+          enablePan={false}
+          // set the min and max polar angle (vertical height) for the camera
+          minPolarAngle={degToRad(0)}
+          maxPolarAngle={degToRad(90)}
+          // set the min and max zoom distances for the camera
+          minDistance={1}
+          maxDistance={3}
         />
-      <ambientLight intensity={1.0}/>
-      <ElephantModel />
-      <mesh rotation-x={-(Math.PI * .5)} >
-        <planeGeometry/>
-        <meshBasicMaterial color="lightblue" />
-      </mesh>
-    </Canvas>
-        </div>
+        <XR
+          referenceSpace="local-floor"
+          onSessionStart={() => setIsImmersed(true)}
+          onSessionEnd={() => setIsImmersed(false)}
+        >
+          <Controllers />
+          <ambientLight intensity={1.0} />
+          <ElephantModel />
+          <mesh rotation-x={-(Math.PI * 0.5)}>
+            <planeGeometry />
+            <meshBasicMaterial color="lightblue" />
+          </mesh>
+        </XR>
+      </Canvas>
+    </div>
   );
 }
